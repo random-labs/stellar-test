ba8e940f631ff056200733db301227a5bc63b5d6
Ludovic Lerus <lerus_l@epitech.eu>
2018-07-17 21:00:19 +0200

    feat(env): NODE_ENV added

--- src/env.js
+++ src/env.js
@@ -1,4 +1,5 @@
 module.exports = {
+	NODE_ENV:            process.env.NODE_ENV || 'development',
 	PORT:                process.env.PORT || 3000,
 	STELLAR_PUBLIC_KEY:  process.env.STELLAR_PUBLIC_KEY || null,
 	STELLAR_PRIVATE_KEY: process.env.STELLAR_PRIVATE_KEY || null,
